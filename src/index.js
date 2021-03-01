cdcd0134bbf77b0f388b23778236be06e9c8fa53
denvysotskyi <denvysotskyi@gmail.com>
2021-03-01 15:00:47 +0200

    Add a post-add-form-component

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/app';
-import AppHeader from './components/app-header';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
