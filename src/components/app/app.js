043ad4739ad118f468ba23a4016a9085aec33c3f
denvysotskyi <denvysotskyi@gmail.com>
2021-03-02 11:13:50 +0200

    Add a state

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -11,9 +11,9 @@ import './app.css';
 const App = () => {
 
   const data = [
-    {label: 'Going to learn React', important: true},
-    {label: 'That is so good', important: false},
-    {label: 'I need a break...', important: false}
+    {label: 'Going to learn React', important: true, id: 'qwqw'},
+    {label: 'That is so good', important: false, id: 'werr'},
+    {label: 'I need a break...', important: false, id: 'fdfdf'}
   ];
 
   return (
--- src/components/post-list/post-list.js
+++ src/components/post-list/post-list.js
@@ -7,15 +7,13 @@ import './post-list.css';
 const PostList = ({posts}) => {
 
   const elements = posts.map((item) => {
+    const {id, ...itemProps} = item;
     return (
-      <li className="list-group-item">
-        <PostListItem
-        label={item.label} 
-        important={item.important} />
+      <li key={id} className="list-group-item">
+        <PostListItem {...itemProps} />
       </li>
     )
   });
-
 
   return (
     <ul className="app-list list-group">
